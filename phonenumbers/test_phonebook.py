c481b010fa6e115c19c183bc27201f539ba61652
Jeanine Schoessler <jeanine.mt@gmail.com>
2022-02-19 10:21:55 -0700

    add examples with setUp

--- phonenumbers/test_phonebook.py
+++ phonenumbers/test_phonebook.py
@@ -2,17 +2,34 @@ import unittest
 
 from phonebook import Phonebook
 
+
 class PhonebookTest(unittest.TestCase):
+    # Constructs a Phonebook object in each test method.
+    def setUp(self) -> None:
+        self.phonebook = Phonebook()
+
+    # Removes resources if needed, 
+    # but phonebook is in memory so unnecessary
+    def tearDown(self) -> None:
+        pass
+
     def test_lookup_by_name(self):
-        phonebook = Phonebook()
-        phonebook.add('Bob', '12345')
-        number = phonebook.lookup('Bob')
+        self.phonebook.add('Bob', '12345')
+        number = self.phonebook.lookup('Bob')
         self.assertEqual(number, '12345')
 
-    # def test_missing_name(self):
-    #     phonebook = Phonebook()
-    #     with self.assertRaises(KeyError):
-    #         phonebook.lookup('missing')
+    def test_missing_name(self):
+        self.phonebook = Phonebook()
+        # lines enclosed in with function will throw key error
+        # this WILL throw an error to pass
+        with self.assertRaises(KeyError):
+            self.phonebook.lookup('missing')
+
+    # @unittest.skip("WIP")
+    def test_empty_phonebook_is_consistent(self):
+        self.phonebook = Phonebook()
+        # is_consistent method will return True
+        self.assertTrue(self.phonebook.is_consistent())
 
     # def test_is_consistent(self):
     #     phonebook = Phonebook()
--- readme.md
+++ readme.md
@@ -3,3 +3,20 @@
 ```py
 python3 -m unittest
 ```
+
+You can skip a test case by adding a line above the def:
+
+```py
+@unittest.skip("WIP")
+def test_name(self):
+```
+
+Maintain a good test suite:
+
+All the test cases are using `phonebook = Phonebook()`, is there a way to clean this up?
+
+Use Test Fixture:
+
+1. setUp()
+1. TestCaseMethod()
+1. tearDown()
