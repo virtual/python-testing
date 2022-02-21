270554308baa45a8b013733566088901852d5df7
Jeanine Schoessler <jeanine.mt@gmail.com>
2022-02-21 16:29:54 -0700

    add tests, info

--- phonenumbers/test_phonebook.py
+++ phonenumbers/test_phonebook.py
@@ -19,25 +19,32 @@ class PhonebookTest(unittest.TestCase):
         self.assertEqual(number, '12345')
 
     def test_missing_name(self):
-        self.phonebook = Phonebook()
         # lines enclosed in with function will throw key error
         # this WILL throw an error to pass
         with self.assertRaises(KeyError):
             self.phonebook.lookup('missing')
 
-    # @unittest.skip("WIP")
     def test_empty_phonebook_is_consistent(self):
-        self.phonebook = Phonebook()
-        # is_consistent method will return True
         self.assertTrue(self.phonebook.is_consistent())
 
-    # def test_is_consistent(self):
-    #     phonebook = Phonebook()
-    #     phonebook.add('Bob', '12345')
-    #     self.assertTrue(phonebook.is_consistent())
-    #     phonebook.add('Mary', '012345')
-    #     self.assertTrue(phonebook.is_consistent())
-    #     phonebook.add('Sue', '12345')
-    #     self.assertTrue(phonebook.is_consistent())
-    #     phonebook.add('Sue', '123')
-    #     self.assertFalse(phonebook.is_consistent())
+    def test_phonebook_with_some_names_is_consistent(self):
+        self.phonebook.add('Bob', '12345')
+        self.phonebook.add('Mary', '012345')
+        self.phonebook.add('Sue', '12345')
+        self.assertTrue(self.phonebook.is_consistent())
+
+    @unittest.skip("WIP")
+    def test_phonebook_with_duplicate_names_is_inconsistent(self):
+        self.phonebook.add('Bob', '12345')
+        self.phonebook.add('Mary', '012345')
+        self.phonebook.add('Sue', '12345')
+        self.phonebook.add('Bob', '123')
+        self.assertFalse(self.phonebook.is_consistent())
+
+    @unittest.skip("WIP")
+    def test_phonebook_is_inconsistent_with_duplicate_prefix(self):
+        self.phonebook.add('Bob', '12345')
+        self.phonebook.add('Mary', '12345')
+        self.phonebook.add('Sue', '12345')
+        self.phonebook.add('Mike', '123')
+        self.assertFalse(self.phonebook.is_consistent())
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 python3 -m unittest
 ```
 
+[unittest docs](https://docs.python.org/3/library/unittest.html)
+
 You can skip a test case by adding a line above the def:
 
 ```py
@@ -20,3 +22,11 @@ Use Test Fixture:
 1. setUp()
 1. TestCaseMethod()
 1. tearDown()
+
+Test structure:
+
+- Arrange: add entries
+- Act: check using method
+- Assert: whether the act returns correctly (true / false)
+
+Each test should be its own definition, if you're using comments to explain, it probably needs to be broken up into more defs
